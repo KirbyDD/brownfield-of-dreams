703d34de126d9109a380c1cb3b9c8c04df478a39
Daniel <danmoran.pro@gmail.com>
2020-04-01 19:21:10 -0600

    Friendships complete

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,6 +1,5 @@
 class FriendshipsController < ApplicationController
   def create
-    binding.pry
     user = User.find_by(uid: params[:uid])
     @friendship = current_user.friendships.create!(friend_id: user.id)
     if @friendship.save
@@ -8,7 +7,6 @@ class FriendshipsController < ApplicationController
     else
       flash[:error] = "Unable to add friend."
     end
-    binding.pry
     redirect_to "/dashboard"
   end
 end
