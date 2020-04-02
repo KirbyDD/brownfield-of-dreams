bad2aab4bbcfc1e7e58db60ab37a772fdde6e060
Daniel <danmoran.pro@gmail.com>
2020-04-01 20:05:32 -0600

    added call back route

--- config/routes.rb
+++ config/routes.rb
@@ -7,23 +7,25 @@ Rails.application.routes.draw do
   end
 
   root 'welcome#index'
-  get '/auth/github/callback', to: 'signup#create'
   get 'tags/:tag', to: 'welcome#index', as: :tag
   get '/register', to: 'users#new'
-
+  
   namespace :admin do
     get "/dashboard", to: "dashboard#show"
     resources :tutorials, only: [:create, :edit, :update, :destroy, :new] do
       resources :videos, only: [:create]
     end
     resources :videos, only: [:edit, :update, :destroy]
-
+    
     namespace :api do
       namespace :v1 do
         put "tutorial_sequencer/:tutorial_id", to: "tutorial_sequencer#update"
       end
     end
   end
+  
+  get '/auth/github', as: 'github_login'
+  get '/auth/github/callback', to: 'signup#create'
 
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
